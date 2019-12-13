c2cfeed17c94ced8338f95866c6d779a920494a9
Xeniya Denissova <Xeniya_Denissova@epam.com>
2019-12-13 09:50:23 +0600

    Add Password generation and Dump analyze tasks

---
+++ PasswordGeneration/PasswordGeneration/PasswordGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGeneration
+{
+    public class PasswordGenerator
+    {
+        const int iterate = 10000;
+
+        public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+
+
+            return passwordHash;
+
+        }
+    }
+}
---
+++ PasswordGeneration/PasswordGeneration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PasswordGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passwordGenerator = new PasswordGenerator();
+            var salt = new byte[] { 10, 15, 23, 76, 23, 56, 98, 76, 87, 90, 45, 56, 87, 02, 99, 56,54, 76 };
+            var password = passwordGenerator.GeneratePasswordHashUsingSalt("Qe2!w3Epsf_4r@", salt);
+            Console.WriteLine(password);
+            var password2 = passwordGenerator.GeneratePasswordHashUsingSalt("Qe2!w3Epffddsf_4r@", salt);
+            Console.WriteLine(password2);
+        }
+    }
+}
